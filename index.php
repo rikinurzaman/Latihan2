f75f38184c5f03938feadbf68e7d4fc7bdbb1acb
Herdiana <Herdiana@Herdiana-PC>
2014-12-17 10:49:39 +0700

    1.3

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         echo 'Latihan sob';
-        echo 'da aku mah apa atuh'
+        echo 'da aku mah apa atuh update'
         ?>
     </body>
 </html>
